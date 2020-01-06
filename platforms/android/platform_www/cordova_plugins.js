5039449bf3a9eacac907f98ddd7aa72c74673748
miqueias91 <miqueiasmcaetano@gmail.com>
2020-01-05 21:08:29 -0300

    ok

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -55,6 +55,27 @@ module.exports = [
     "merges": [
       "navigator.app"
     ]
+  },
+  {
+    "id": "cordova-promise-polyfill.Promise",
+    "file": "plugins/cordova-promise-polyfill/www/Promise.js",
+    "pluginId": "cordova-promise-polyfill",
+    "runs": true
+  },
+  {
+    "id": "cordova-promise-polyfill.promise.min",
+    "file": "plugins/cordova-promise-polyfill/www/promise.min.js",
+    "pluginId": "cordova-promise-polyfill"
+  },
+  {
+    "id": "cordova-plugin-admob-free.AdMob",
+    "file": "plugins/cordova-plugin-admob-free/www/admob.js",
+    "pluginId": "cordova-plugin-admob-free",
+    "clobbers": [
+      "admob",
+      "AdMob",
+      "plugins.AdMob"
+    ]
   }
 ];
 module.exports.metadata = 
@@ -67,7 +88,10 @@ module.exports.metadata =
   "kr.co.joycorp.cordova.exitapp": "1.0.2",
   "cordova-plugin-splashscreen": "5.0.2",
   "onesignal-cordova-plugin": "2.4.6",
-  "cordova.custom.plugins.exitapp": "1.0.0"
+  "cordova.custom.plugins.exitapp": "1.0.0",
+  "cordova-promise-polyfill": "0.0.2",
+  "cordova-admob-sdk": "0.24.1",
+  "cordova-plugin-admob-free": "0.27.0"
 };
 // BOTTOM OF METADATA
 });
